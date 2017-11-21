0c6324aba89f7339ed574609d3079c1e8872006c
Shikha <shikha@onmobile.com>
2017-11-21 22:48:05 +0530

    '60commit'

--- angular/controller/Second.js
+++ angular/controller/Second.js
@@ -3,12 +3,13 @@ myApp.controller("secondController", ['$http','$location','$routeParams',functio
 	var main=this;
 	this.baseUrl = 'https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json';
 	this.baseUrl2= 'https://raw.githubusercontent.com/openfootball/football.json/master/2016-17/en.1.json';
+	//Declaring Required variables
 	this.matchId1 = $routeParams.matchid1 ;
     this.matchId2 = $routeParams.matchid2 ;
     this.matchDate = $routeParams.matchdate ;
-	console.log(main.matchId1);
-	console.log(main.matchId2);
-	console.log(main.matchDate);
+	//console.log(main.matchId1);
+	//console.log(main.matchId2);
+	//console.log(main.matchDate);
 	this.team1;
     this.team2;
     this.score1;
@@ -19,7 +20,7 @@ myApp.controller("secondController", ['$http','$location','$routeParams',functio
 	this.code1;
 	this.code2;
     this.data = [];
-	this.loadsingleData=function(data)
+	this.loadsingleData=function(data)  //load Single detail Match Function
 	{
 		for(var i in main.data)
 		{
@@ -29,42 +30,42 @@ myApp.controller("secondController", ['$http','$location','$routeParams',functio
 				{
 					console.log("Record_Found");
 					main.day=main.data[i].name;
-					console.log(main.data[i].name);
-					console.log(main.day);
+					//console.log(main.data[i].name);
+					//console.log(main.day);
 					main.team1=main.data[i].matches[j].team1.name;
-					console.log(main.data[i].matches[j].team1.name);
-					console.log(main.team1);
+					//console.log(main.data[i].matches[j].team1.name);
+					//console.log(main.team1);
 					main.team2=main.data[i].matches[j].team2.name;
-					console.log(main.team2);
-					console.log(main.data[i].matches[j].team2.name);
+					//console.log(main.team2);
+					//console.log(main.data[i].matches[j].team2.name);
 					main.score1=main.data[i].matches[j].score1;
-					console.log(main.score1);
-					console.log(main.data[i].matches[j].score1);
+					//console.log(main.score1);
+					//console.log(main.data[i].matches[j].score1);
 					main.score2=main.data[i].matches[j].score2;
-					console.log(main.data[i].matches[j].score2);
-					console.log(main.score2);
+					//console.log(main.data[i].matches[j].score2);
+					//console.log(main.score2);
 					main.date=main.data[i].matches[j].date;
-					console.log(main.data[i].matches[j].date);
-					console.log(main.date);
+					//console.log(main.data[i].matches[j].date);
+					//console.log(main.date);
 					main.code1=main.data[i].matches[j].team1.code;
-					console.log(main.data[i].matches[j].team1.code);
-					console.log(main.code1);
+					//console.log(main.data[i].matches[j].team1.code);
+					//console.log(main.code1);
 					main.code2=main.data[i].matches[j].team2.code;
-					console.log(main.data[i].matches[j].team2.code);
-					console.log(main.code2);
+					//console.log(main.data[i].matches[j].team2.code);
+					//console.log(main.code2);
 					if(main.score1=main.score2)
 					{
 						main.winner="Match Drawn";
-						console.log(main.winner);
+						//console.log(main.winner);
 					}
 					else if (main.score1>main.score2)
 					{
 						main.winner= ""+main.team1+" wins !!!! and dominates over "+main.team2;
-						console.log(main.winner);
+						//console.log(main.winner);
 					}
 						else{
 						  main.winner = ""+main.team2+" wins!! and dominates over"+main.team1;
-                         console.log(main.winner);						  
+                         //console.log(main.winner);						  
 					}
 				}
 			}
@@ -77,7 +78,7 @@ myApp.controller("secondController", ['$http','$location','$routeParams',functio
 			  url:main.baseUrl
 			  }).then(function successCallback(response)
 			  {
-				  console.log("working");
+				  //console.log("working");
 				  main.data = response.data.rounds;
 				main.loadsingleData(main.data); 
 			  },function errorcallBack(reason)
@@ -94,7 +95,7 @@ myApp.controller("secondController", ['$http','$location','$routeParams',functio
 			  url:main.baseUrl2
 			  }).then(function successCallback(response)
 			  {  
-			    console.log("working");
+			    //console.log("working");
 				 main.data = response.data.rounds;
 				main.loadsingleData(main.data); 
 			  },function errorcallBack(reason)
--- angular/controller/third.js
+++ angular/controller/third.js
@@ -28,7 +28,7 @@ myApp.controller("StatsController",["$http",function($http){
 
                     if (main.rounds1[i].matches[j].team1.name == data1){
                       
-                       code1 = main.rounds1[i].matches[j].team1.name;
+                       code1 = main.rounds1[i].matches[j].team1.code;
 
                        playedmatches++ ; 
 
@@ -53,7 +53,7 @@ myApp.controller("StatsController",["$http",function($http){
                     
                     else if(main.rounds1[i].matches[j].team2.name == data1){
 
-                       code1 = main.rounds1[i].matches[j].team2.name ;
+                       code1 = main.rounds1[i].matches[j].team2.code ;
                       
                        playedmatches++ ;
 
@@ -93,83 +93,50 @@ myApp.controller("StatsController",["$http",function($http){
 
 
            this.statsCheck = function(data1,year1){ 
-
-              if (year1== '2015'){
-                   $http({
+           if(data1== "" && year1 == " ")
+		   {
+			   alert("Please enter mentioned details");
+		   }
+		   else if(data1=="" || year1 == "")
+		   {
+			  alert("Please enter all details"); 
+		   }
+		   else if( year1 == "2015")
+		   {
+			   $http({
                     method:"GET",
                     url: main.baseUrl1
               }).then(function successCallback(response){
-
-                this.names1=[];
-
-               for(var i=0; i<response.data.rounds[0].matches.length;i++){
-             //   console.log(response.data.rounds[0].matches.length);
-                  this.names1.push(response.data.rounds[0].matches[i].team1.name);
-                    }
-                  //  console.log(this.names1)
-                for(var i=0;i<response.data.rounds[0].matches.length;i++){
-                  this.names1.push(response.data.rounds[0].matches[i].team2.name);
-                    }
-
-               if(this.names1.includes(data1)){
-                  if(main.value2){               // toggling 2015/16 stats button
-                    main.value2 = false;   
-                     main.value1 = true;
-                    }
-                else{
-                    main.value1 = true;
-                  } 
-                   main.teamStats(response,data1);
-                   }
-                   else{
-                    alert("Team didn't play in 2015 !");
-                   // main.value1 = false;
-                   }      
-
-             }, function errorCallback(reason){   
-                alert("Error in GET");
-                  })
-			  }				  
-                else if(year1==2016) {
-              $http({
+				this.names1=[];  
+				for(var i in  response.data.rounds[0].matches)
+				{
+					this.names1.push(response.data.rounds[0].matches[i].team1.name)
+				}
+				{
+					if(this.names1.includes(data1))
+					{
+						main.teamStats(response,data1)
+					}
+				}
+		   })
+		   }
+		   else if( year1 == "2016")
+		   {
+			   $http({
                     method:"GET",
                     url: main.baseUrl2
               }).then(function successCallback(response){
-             
-              this.names2=[];
-
-               for(var i=0;i<response.data.rounds[0].matches.length;i++){
-         
-                  this.names2.push(response.data.rounds[0].matches[i].team1.name);
-
-                    }
-                for(var i=0;i<response.data.rounds[0].matches.length;i++){
-         
-                  this.names2.push(response.data.rounds[0].matches[i].team2.name);
-
-                    }
-
-                if(this.names2.includes(data1)){
-
-                  if(main.value1 == true){
-                          main.value1 = false ;
-                           main.value2 = true;
-                       }
-                      else {
-                    main.value2 = true;
-                  }
-
-                   main.teamStats(response,data1);
-                   }
-
-                   else{
-                    alert("Team didn't play in 2016 !");
-                   }
-
-              }, function errorCallback(reason){   
-                alert("Error in GET");
-                  })         
-                }   
-             } ;                          
-
+				this.names1=[];  
+				for(var i in  response.data.rounds[0].matches)
+				{
+					this.names1.push(response.data.rounds[0].matches[i].team1.name)
+				}
+				{
+					if(this.names1.includes(data1))
+					{
+						main.teamStats(response,data1)
+					}
+				}
+		   })
+		   }
         }]); // controller ends
